e69750fe1675ae420c6488463b8fcfd888294f31
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-10-14 03:35:58 -0700

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,33 @@
 #!/usr/bin/python3
-"""Defines a class BaseModel"""
-
+"""Defines class BaseModel"""
 
 import uuid
 from datetime import datetime
 
 
-class BaseModel():
-    """defines all common attributes/methods for other classes"""
-    def __init__(self, *args, **kwargs):
-        """initializes class BaseModel"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-
-    def __str__(self):
-        """should print: [<class name>] (<self.id>) <self.__dict__>"""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+class BaseModel:
+    """Represents class BaseModel"""
+    def __init__(self):
+        """Initializes class BaseModel"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
         """updates the public instance attribute updated_at with
         the current datetime"""
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of
-        the instance"""
-        new_dict = dict(self.__dict__)
-        new_dict['created_at'] = self.__dict__['created_at'].isoformat()
-        new_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
-        new_dict['__class__'] = self.__class__.__name__
-        return (new_dict)
+        """returns a dictionary containing all keys/values of
+        __dict__ of the instance"""
+        instance_dict = self.__dict__.copy()
+        instance_dict['__class__'] = self.__class__.__name__
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
+        return instance_dict
+
+    def __str__(self):
+        """prints: [<class name>] (<self.id>) <self.__dict__>"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
