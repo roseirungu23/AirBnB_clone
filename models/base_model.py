d14cdb3248978e94f1e26d2b0350071556d34e77
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-10-14 03:47:31 -0700

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,19 @@ from datetime import datetime
 
 class BaseModel:
     """Represents class BaseModel"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes class BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """updates the public instance attribute updated_at with
